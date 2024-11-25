ec9ec6394ab7b9d8e12cb187146ae00638536a30
popaa <popaa@Popa>
2024-11-25 18:53:40 +0200

    Am comis si acest commit v3

--- Termopane/src/main/java/ro/ugal/facultate/Facultate.java
+++ Termopane/src/main/java/ro/ugal/facultate/Facultate.java
@@ -1,20 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package ro.ugal.facultate;
 
-/**
- *
- * @author popaa
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Facultate {
     public static void main(String[] args) {
+        List<Termopane> termopaneList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            termopaneList.add(new Termopane("Material" + i, i + 1, "TipSticla" + i));
+        }
+
+        List<TermopanIzolantTermic> izolantList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            izolantList.add(new TermopanIzolantTermic("Material" + i, i + 1, "TipSticla" + i, i * 1.1));
+        }
+
+        List<TermopanReflectorizant> reflectorizantList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            reflectorizantList.add(new TermopanReflectorizant("Material" + i, i + 1, "TipSticla" + i, "Film" + i));
+        }
+
+        System.out.println("Instante Termopane:");
+        for (Termopane termopan : termopaneList) {
+            System.out.println(termopan);
+        }
+
+        System.out.println("\nInstanțe TermopanIzolantTermic:");
+        for (TermopanIzolantTermic izolant : izolantList) {
+            System.out.println(izolant);
+        }
+
+        System.out.println("\nInstante TermopanReflectorizant:");
+        for (TermopanReflectorizant reflectorizant : reflectorizantList) {
+            System.out.println(reflectorizant);
+        }
+
         Termopane t1 = new Termopane();
         Termopane t2 = new Termopane("PVC", 2, "Clara");
         Termopane t3 = new Termopane(t2);
 
-        System.out.println("Instante Termopane:");
+        System.out.println("\nInstante individuale Termopane:");
         System.out.println(t1);
         System.out.println(t2);
         System.out.println(t3);
@@ -23,7 +48,7 @@ public class Facultate {
         TermopanIzolantTermic iz2 = new TermopanIzolantTermic("Aluminiu", 3, "Fumuriu", 1.2);
         TermopanIzolantTermic iz3 = new TermopanIzolantTermic(iz2);
 
-        System.out.println("\nInstante TermopanIzolantTermic:");
+        System.out.println("\nInstante individuale TermopanIzolantTermic:");
         System.out.println(iz1);
         System.out.println(iz2);
         System.out.println(iz3);
@@ -32,11 +57,9 @@ public class Facultate {
         TermopanReflectorizant rf2 = new TermopanReflectorizant("Lemn", 2, "Fumuriu", "UV");
         TermopanReflectorizant rf3 = new TermopanReflectorizant(rf2);
 
-        System.out.println("\nInstanțe TermopanReflectorizant:");
+        System.out.println("\nInstante individuale TermopanReflectorizant:");
         System.out.println(rf1);
         System.out.println(rf2);
         System.out.println(rf3);
     }
 }
-
-
